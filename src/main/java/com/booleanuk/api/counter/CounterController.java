306c8699d4f54beb4e33daa5275a4656a48fc9fa
Matilda Sönnergaard <matilda.sonnergaard@se.experis.com>
2025-01-16 11:35:03 +0100

    Change GET methods to return String instead of int and store counter in Map instead of single int

--- src/main/java/com/booleanuk/api/counter/CounterController.java
+++ src/main/java/com/booleanuk/api/counter/CounterController.java
@@ -1,30 +1,44 @@
 package com.booleanuk.api.counter;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/counter")
 public class CounterController {
-    private int value;
+    private String currentCounter;
+    private Map<String, Integer> counters;
 
     public CounterController() {
-        this.value = 0;
+        this.currentCounter = "default counter";
+        this.counters = new HashMap<>();
+        this.counters.put(currentCounter, 0);
+    }
+
+    @GetMapping("/custom/{name}")
+    public String changeToCustomCounter(@PathVariable String counter) {
+        return "";
     }
 
     @GetMapping("/increment")
-    public int increment() {
-        return ++this.value;
+    public String increment() {
+        this.counters.put(currentCounter, this.counters.get(currentCounter) + 1);
+        return String.valueOf(this.counters.get(currentCounter));
     }
 
     @GetMapping("/decrement")
-    public int decrement() {
-        return --this.value;
+    public String decrement() {
+        this.counters.put(currentCounter, this.counters.get(currentCounter) - 1);
+        return String.valueOf(this.counters.get(currentCounter));
     }
 
     @GetMapping
-    public int getValue() {
-        return this.value;
+    public String getValue() {
+        return String.valueOf(this.counters.get(currentCounter));
     }
 }
